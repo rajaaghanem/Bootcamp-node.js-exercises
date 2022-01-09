d863c40129ba862d713596bfbd18466f54d0eda9
rajaaghanem <rajaaghanem@gmail.com>
2022-01-09 13:25:15 +0200

    first

---
+++ 2.1-file_system/ex-2.1.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+fs.writeFileSync("notes.txt", "My first node.js file");
+
+//Copy file function
+
+function callback(err) {
+  if (err) throw err;
+  console.log("source.txt was copied to destination.txt");
+}
+fs.copyFile("notes.txt", "copied_notes.txt", callback);
+
+//Rename file function
+
+fs.rename("copied_notes.txt", "newName_copied_notes.txt", (err) => {
+  if (err) throw err;
+  console.log("Rename complete!");
+});
+
+//Get a list of all the file names of the current directory
+
+const testFolder = '../2.1-file_system/';
+fs.readdirSync(testFolder).forEach((file) => {
+  console.log(file);
+});
+
+// 
+fs.appendFileSync('notes.txt', ' adding data');
