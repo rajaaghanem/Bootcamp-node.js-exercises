569f0508e38dc41cf7888ff1fd0fab3842c5daf9
rajaaghanem <rajaaghanem@gmail.com>
2022-01-10 11:44:22 +0200

    mult and pow

--- 4.1-calculator/ex-4.1.js
+++ 4.1-calculator/ex-4.1.js
@@ -1,20 +1,46 @@
 import yargs from "yargs";
-import { hideBin } from 'yargs/helpers';
+import { hideBin } from "yargs/helpers";
 
 //! add
-yargs(hideBin(process.argv)).command({
-  command: "add",
-  describe: "addition",
-  handler: (argv) => {
-      console.log((argv._[1]+argv._[2]));
-  },
-}).parse();
+yargs(hideBin(process.argv))
+  .command({
+    command: "add",
+    describe: "addition",
+    handler: (argv) => {
+      console.log(argv._[1] + argv._[2]);
+    },
+  })
+  .parse();
 
 //! substract
-yargs(hideBin(process.argv)).command({
+yargs(hideBin(process.argv))
+  .command({
     command: "sub",
     describe: "substract",
     handler: (argv) => {
-        console.log((argv._[1]-argv._[2]));
+      console.log(argv._[1] - argv._[2]);
     },
-  }).parse();
+  })
+  .parse();
+
+//! multiply
+yargs(hideBin(process.argv))
+  .command({
+    command: "mult",
+    describe: "multiply",
+    handler: (argv) => {
+      console.log(argv._[1] * argv._[2]);
+    },
+  })
+  .parse();
+
+//!pow
+yargs(hideBin(process.argv))
+  .command({
+    command: "pow",
+    describe: "pow",
+    handler: (argv) => {
+      console.log(argv._[1] ** 2);
+    },
+  })
+  .parse();
