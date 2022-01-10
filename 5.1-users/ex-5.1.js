5cc0143407527ba9ee4b2a8409561f70a289b410
rajaaghanem <rajaaghanem@gmail.com>
2022-01-10 15:01:49 +0200

    update user info function

--- 5.1-users/ex-5.1.js
+++ 5.1-users/ex-5.1.js
@@ -1,27 +1,27 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
-import {addUser, removeUser} from "./users.js";
+import { addUser, removeUser, updateUser } from "./users.js";
 
 //create add user
 yargs(hideBin(process.argv))
   .command({
     command: "create",
     describe: "create new user",
-    builder:{
-        id:{
-            describe: "user id",
-            demandOptions: false,
-            type: "string"
-        },
-        name:{
-            describe: "user name",
-            demandOptions: true,
-            type: "string"
-        },
-        email:{
-          describe: "user email",
-          demandOptions: true,
-          type: "string"
+    builder: {
+      id: {
+        describe: "user id",
+        demandOptions: false,
+        type: "string",
+      },
+      name: {
+        describe: "user name",
+        demandOptions: true,
+        type: "string",
+      },
+      email: {
+        describe: "user email",
+        demandOptions: true,
+        type: "string",
       },
     },
     handler: (argv) => {
@@ -30,16 +30,17 @@ yargs(hideBin(process.argv))
   })
   .parse();
 
-  yargs(hideBin(process.argv))
+  //remove a user by id
+yargs(hideBin(process.argv))
   .command({
     command: "delete",
     describe: "delete a user",
-    builder:{
-        id:{
-            describe: "user id",
-            demandOptions: false,
-            type: "string"
-        },
+    builder: {
+      id: {
+        describe: "user id",
+        demandOptions: true,
+        type: "string",
+      },
     },
     handler: (argv) => {
       removeUser(argv.id);
@@ -47,5 +48,30 @@ yargs(hideBin(process.argv))
   })
   .parse();
 
-
-
+  //update a user by id
+yargs(hideBin(process.argv))
+  .command({
+    command: "update",
+    describe: "update a user",
+    builder: {
+      id: {
+        describe: "user id",
+        demandOptions: true,
+        type: "string",
+      },
+      name: {
+        describe: "user name",
+        demandOptions: false,
+        type: "string",
+      },
+      email: {
+        describe: "user email",
+        demandOptions: false,
+        type: "string",
+      },
+    },
+    handler: (argv) => {
+      updateUser(argv.id, argv.name, argv.email);
+    },
+  })
+  .parse();
--- 5.1-users/users.js
+++ 5.1-users/users.js
@@ -1,36 +1,56 @@
 import fs from "fs";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 //add a user
 export function addUser(userName, userEmail) {
-    const users = loadUsers();
+  const users = loadUsers();
 
-    users.push({
-        id:uuidv4(),
-        name: userName,
-        email: userEmail,
-    })
+  users.push({
+    id: uuidv4(),
+    name: userName,
+    email: userEmail,
+  });
 
-    saveUser(users)
-    // console.log(users);
+  saveUser(users);
 }
 
 //delete a user
 
-export function removeUser(userID){
-    const users = loadUsers();
+export function removeUser(userID) {
+  const users = loadUsers();
 
-    const newUsers= users.filter((user)=> {
-        return user.id !== userID
+  const newUsers = users.filter((user) => {
+    return user.id !== userID;
+  });
+
+  saveUser(newUsers);
+}
+
+export function updateUser(userID, userName, userEmail) {
+  const users = loadUsers();
+
+  const theUser = users.find((user) => {
+    return user.id === userID;
+  });
+
+  if (theUser) {
+   const editedUser = {
+      ...theUser,
+      id: userID,
+      name: userName,
+      email: userEmail,
+    };
+    const newUsers = users.map((user) => {
+      return user.id === userID ? editedUser : user;
     });
-
     saveUser(newUsers);
+  }
 }
 
-const saveUser=(users)=>{
-    const dataJson= JSON.stringify(users);
-    fs.writeFileSync('users.json', dataJson);
-}
+const saveUser = (users) => {
+  const dataJson = JSON.stringify(users);
+  fs.writeFileSync("users.json", dataJson);
+};
 
 const loadUsers = () => {
   try {
@@ -41,7 +61,3 @@ const loadUsers = () => {
     return [];
   }
 };
-
-
-
-
