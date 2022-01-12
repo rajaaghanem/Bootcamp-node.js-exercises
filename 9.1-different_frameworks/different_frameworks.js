b66de30444a3aea1273096bc69f1828a60bc08ba
rajaaghanem <rajaaghanem@gmail.com>
2022-01-12 10:23:10 +0200

    meteor

---
+++ 9.1-different_frameworks/different_frameworks.js
@@ -0,0 +1,21 @@
+//! Koa: Utilizing Next Generation JavaScript Features
+
+//what makes Koa truly standout is its way to ditch callback
+// completely by using the ES6 Generator feature.
+
+//not bundling any middleware within its core - Middleware are basically just functions that have full access to both request and response objects.
+
+//!Meteor: Framework For Web, Mobile And Desktop
+
+
+//a full-stack framework, a full package that covers server, mobile, desktop and web apps.
+//One of the features that distinguishes Meteor from other frameworks is its data synchronization
+// mechanism among server and front-end/mobile app.
+//Meteor uses data on the wire, meaning the server sends data, not HTML, and the client renders it.
+
+
+
+//* a cost of jeopardized performance and scalability restrictions
+//* at some point the system architecture has to be redesigned in a more professional way if the prototype 
+//* becomes a production project with enough client base.
+
